351582c3cb7adf2007484fa390a02a58ad658ad0
hossain <guljer.net@gmail.com>
2024-05-26 17:48:28 +0600

    data save to server

--- src/Dashboard/All-Food/AddFood.jsx
+++ src/Dashboard/All-Food/AddFood.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 function AddFood() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = data => {
+    console.log(data);
+  };
+
   return (
     <div>
       <div className="flex items-center justify-between">
@@ -14,21 +25,59 @@ function AddFood() {
         </Link>
       </div>
       <div className="mt-5">
-        <form action="">
+        <form action="" onSubmit={handleSubmit(onSubmit)}>
           <div>
-            <input type="text" placeholder="Enter Food Name" className="w-full border border-primary outline-0 rounded mb-3 px-3 py-2" />
+            <input
+              type="text"
+              placeholder="Enter Food Name"
+              {...register("title", { required: true })}
+              className="w-full border border-primary outline-0 rounded mb-3 px-3 py-2"
+            />
+            {errors.title?.type === "required" && (
+              <p className="text-red-600 py-2">Product Title is Required</p>
+            )}
           </div>
           <div>
-            <input type="number" placeholder="Price" className="w-full border border-primary outline-0 rounded mb-3 px-3 py-2" />
+            <input
+              type="number"
+              placeholder="Price"
+              {...register("price", { required: true })}
+              className="w-full border border-primary outline-0 rounded mb-3 px-3 py-2"
+            />
+            {errors.price?.type === "required" && (
+              <p className="text-red-600 py-2">Product Price is Required</p>
+            )}
           </div>
           <div>
-            <textarea name="" id="" placeholder="Description Here..." className="w-full border mb-3 border-primary resize-none h-[160px] p-3 rounded outline-0"></textarea>
+            <textarea
+              name=""
+              id=""
+              placeholder="Description Here..."
+              {...register("description", { required: true })}
+              className="w-full border mb-3 border-primary resize-none h-[160px] p-3 rounded outline-0"
+            ></textarea>
+            {errors.description?.type === "required" && (
+              <p className="text-red-600 py-2">
+                Product Description is Required
+              </p>
+            )}
           </div>
           <div>
-            <input type="file" className="w-full border border-primary outline-0 rounded mb-3 px-3 py-2" />
+            <input
+              type="file"
+              {...register("image", { required: true })}
+              className="w-full border border-primary outline-0 rounded mb-3 px-3 py-2"
+            />
+            {errors.image?.type === "required" && (
+              <p className="text-red-600 py-2">Image is Required</p>
+            )}
           </div>
           <div>
-            <input type="submit" value="Add Food" className="px-5 py-2 bg-primary text-white rounded" />
+            <input
+              type="submit"
+              value="Add Food"
+              className="px-5 py-2 bg-primary text-white rounded"
+            />
           </div>
         </form>
       </div>
